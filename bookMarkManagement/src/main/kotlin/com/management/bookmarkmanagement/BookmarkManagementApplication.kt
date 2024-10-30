c74896fe2b1c7267a25e7a129dbdf73d5603286a
hyeyeon <gpdus335@gmail.com>
2024-10-31 01:23:04 +0900

    (project)chore: 프로젝트 셋팅 - 환경셋팅 - 도커 이미지 생성 및 gradle 셋팅

---
+++ bookMarkManagement/src/main/kotlin/com/management/bookmarkmanagement/BookmarkManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.management.bookmarkmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookmarkManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookmarkManagementApplication>(*args)
+}
